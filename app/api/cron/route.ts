6bbf14b2fc41c8299a4ffd01fb004b7446c8e4a5
Sumit <marvelsumit2002@gmail.com>
2024-02-03 13:14:28 +0530

    fixing deployment errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET() {
     try {
         connectToDB()
@@ -34,9 +38,8 @@ export async function GET() {
                   }
 
                   const updatedProduct = await Product.findOneAndUpdate(
-                    { url: scrapedProduct.url },
+                    { url: product.url },
                     product,
-                    { upsert: true, new: true }
                   );
 
                   // 2. CHECK EACH PRODUCTS'S STATUS & SEND EMAIL ACCORDINGLY
